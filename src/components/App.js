12bb07caad668549fce421e3e4000ec2a5dc4b7b
Dan Smith <denzik@gmail.com>
2021-05-13 14:53:48 -0400

    Fix Dropdowns when positioned on mobile

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import AppHeader from './Header';
 import TranslationsTable from './TranslationsTable';
 import Footer from './Footer';
-import { Popup, Icon, Image} from 'semantic-ui-react';
 import '../styles/App.css'
 
 const App = () => {
