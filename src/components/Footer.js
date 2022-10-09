32b8be1365a9d95b4ac31e9b95fe896fd67e6ed5
Dan Smith <denzik@gmail.com>
2022-10-09 15:02:16 -0400

    Update README and updates for the Farcast/Duskpaw Festival

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
       />
       <Popup
           trigger={<Icon circular name='info circle' size='large' inverted color="black" fitted inline />}
-          content='Last Updated for the Skadi Blöt Festival'
+          content='Last Updated for the Duskpaw-Farcast Blöt Festival'
           position="top right"
           inverted
         />
